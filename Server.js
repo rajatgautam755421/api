7b49ab95910f187f8eb6eaf10d002ebb60cf9982
rajat755421 <rajatgautam832@gmail.com>
2021-10-19 10:35:54 +0545

    1st commit

---
+++ Server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+require("dotenv").config({ path: "" });
+require("./Config/Database");
+
+//Global Level Middlewares
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+
+app.use(logger("dev"));
+
+//Importing Routes
+const contactRoute = require("./Routes/Contact.Route");
+
+//Configuire Routes
+app.use("/api/v1", contactRoute);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "Welcome",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up and running at 3000 🚀 ");
+});
